ba07930539557207b9230f8e3e46edb4ce4b1a84
0JIEUN0 <jieun4436@naver.com>
2021-07-27 18:47:24 +0900

    Fix User Entity (add ID) and Add application.properties file

--- src/main/kotlin/com/rticcle/server/domain/user/User.kt
+++ src/main/kotlin/com/rticcle/server/domain/user/User.kt
@@ -1,8 +1,16 @@
 package com.rticcle.server.domain.user
 import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
 
 @Entity
 class User (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long = Long.MAX_VALUE,
+
     var userEmail: String = "",
     var userPassword: String = ""
-)
+) {
+}
