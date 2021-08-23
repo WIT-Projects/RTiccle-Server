e1925cb50776d97621aa6576cd3c22122edfe150
0JIEUN0 <jieun4436@naver.com>
2021-08-23 16:05:38 +0900

    Fix Kakao Login Error

--- src/main/kotlin/com/rticcle/server/config/auth/dto/OAuthAttributes.kt
+++ src/main/kotlin/com/rticcle/server/config/auth/dto/OAuthAttributes.kt
@@ -17,15 +17,12 @@ class OAuthAttributes (
                attributes: Map<String, Any>): OAuthAttributes {
             if("naver" == registrationId) {
                 // Naver
-                println("naver, $registrationId, $userNameAttributeName")
                 return ofNaver("id", attributes)
-            } else if("Kakao" == registrationId) {
+            } else if("kakao" == registrationId) {
                 // Kakao
-                println("Kakao, $registrationId, $userNameAttributeName")
                 return ofKakao("id", attributes)
             } else {
                 // Google
-                println("google, $registrationId, $userNameAttributeName")
                 return ofGoogle(userNameAttributeName, attributes)
             }
         }
@@ -43,28 +40,32 @@ class OAuthAttributes (
 
         private fun ofNaver(userNameAttributeName: String,
                             attributes: Map<String, Any>): OAuthAttributes {
-            val response: Map<String, Any> = attributes["response"] as Map<String, Any>
+            val naverAttributes: Map<String, Any> = attributes["response"] as Map<String, Any>
 
             return OAuthAttributes(
-                attributes = response,
+                attributes = naverAttributes,
                 nameAttributeKey = userNameAttributeName,
-                name = attributes["name"].toString(),
-                email = attributes["email"].toString(),
-                picture = attributes["profile_image"].toString(),
+                name = naverAttributes["name"].toString(),
+                email = naverAttributes["email"].toString(),
+                picture = naverAttributes["profile_image"].toString(),
             )
         }
 
         private fun ofKakao(userNameAttributeName: String,
                             attributes: Map<String, Any>): OAuthAttributes {
-            val response:Map<String, Any> = attributes["kakao_account"] as Map<String, Any>
-            val profile:Map<String, Any> = response["profile"] as Map<String, Any>
+            val kakaoAccount:Map<String, Any> = attributes["kakao_account"] as Map<String, Any>
+            val kakaoProfile:Map<String, Any> = kakaoAccount["profile"] as Map<String, Any>
+
+            // Add user email to attributes since JWT Token Provider will use "email" property
+            val email: String = kakaoAccount["email"].toString()
+            val returnAttributes: Map<String, Any> = attributes.plus(Pair("email", email))
 
             return OAuthAttributes(
-                attributes = attributes,
+                attributes = returnAttributes,
                 nameAttributeKey = userNameAttributeName,
-                name = profile["nickname"].toString(),
-                email = response["email"].toString(),
-                picture = profile["profile_image_url"].toString(),
+                name = kakaoProfile["nickname"].toString(),
+                email = email,
+                picture = kakaoProfile["profile_image_url"].toString(),
             )
         }
     }
